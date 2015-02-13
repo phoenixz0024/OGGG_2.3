6c9824240a1183284fb8909a09e0cd3a3708fdcb
Kas Feenema <k.feenema@st.hanze.nl>
2015-02-13 21:21:04 +0100

    Opdracht 10 asabi!

--- OG_2.3/src/Test.java
+++ OG_2.3/src/Test.java
@@ -1,15 +1,47 @@
+import java.util.Arrays;
 
-public class Test {
+public class Test 
+{
+	public static void main(String[] args)
+	{
+		int[] a = {1,2,3,2};
+		int[] b = {4,5,2,1};
+		
+		System.out.println(Arrays.toString( sumArrays(a,b) ));
+	}
 	
+	private static int[] sumArrays(int[] a, int[] b)
+	{
+		int[] answer;
+		
+		try
+		{
+			if(a.length!=b.length)
+				throw new ArraySizeException("Array sizes are not equal");
+			
+			if(a.length<b.length)
+				answer = new int[a.length];
+			else
+				answer = new int[b.length];
+			
+			for(int x = 0; x < answer.length; x++) 
+			{
+			  answer[x] = a[x] + b[x];
+			}
+			
+			return answer;
+		}
+		catch(ArraySizeException e)
+		{
+			return null;
+		}
+	}
 	
-	public static void main(String[] args){
-		
-		// This is a comment
-		System.out.println("test");
-		
-		// laatste test
-		System.out.println("Sloerie");
-		
-		
+	private static class ArraySizeException extends Exception
+	{
+		 private ArraySizeException(String message)
+		 {
+		      super(message);
+		 }
 	}
 }
