7d1056a70a8fbf22e1941281d02c18b5d0e6b320
Kas Feenema <k.feenema@st.hanze.nl>
2015-02-14 15:24:10 +0100

    Junit Tests fixed

---
+++ Opdracht8_Leertaak1/src/DobbelsteenMVC.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DobbelsteenMVC extends JApplet
+{
+	DobbelsteenModel model;             //het model
+	TekstView tekstView;              // view
+	DobbelsteenView dobbelsteenView;  // view
+	static StatistiekenView statistiekenView;
+	DobbelsteenController controller;             // Controller
+	
+	public void init()
+	{
+		resize(250,200);
+        
+		// Maak het model
+		model = new DobbelsteenModel();
+        
+        // Maak de controller en geef hem het model
+		controller = new DobbelsteenController(model);
+        controller.setBackground(Color.cyan);
+        getContentPane().add(controller,BorderLayout.NORTH);
+        
+        // Maak de views
+        dobbelsteenView = new DobbelsteenView(Color.red);
+        dobbelsteenView.setBackground(Color.black);
+        getContentPane().add(dobbelsteenView,BorderLayout.CENTER);
+        tekstView = new TekstView();
+        tekstView.setBackground(Color.green);
+        getContentPane().add(tekstView,BorderLayout.SOUTH);
+        statistiekenView = new StatistiekenView();
+        statistiekenView.setBackground(Color.white);
+        getContentPane().add(statistiekenView,BorderLayout.EAST);
+        
+        // Registreer de views bij het model
+        model.addActionListener(tekstView);
+        model.addActionListener(dobbelsteenView);
+        
+        model.gooi();
+        model.addActionListener(statistiekenView);
+	}
+}
---
+++ Opdracht8_Leertaak1/src/StatistiekenView.java
@@ -0,0 +1,95 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class StatistiekenView extends JPanel implements ActionListener
+{
+	DobbelsteenModel d;
+	int waarde;
+	JLabel een, twee, drie, vier, vijf, zes;
+	
+	public StatistiekenView()
+	{
+	    this.setLayout(new GridLayout(7,2));
+	    
+	    this.add(new JLabel("Nummer"));
+	    this.add(new JLabel("Gegooid"));
+	    
+	    this.add(new JLabel("1 :"));
+	    een = new JLabel("0");
+	    this.add(een);
+	   
+	    this.add(new JLabel("2 :"));
+	    twee = new JLabel("0");
+	    this.add(twee);
+	    
+	    this.add(new JLabel("3 :"));
+	    drie = new JLabel("0");
+	    this.add(drie);
+	    
+	    this.add(new JLabel("4 :"));
+	    vier = new JLabel("0");
+	    this.add(vier);
+	    
+	    this.add(new JLabel("5 :"));
+	    vijf = new JLabel("0");
+	    this.add(vijf);
+	    
+	    this.add(new JLabel("6 :"));
+	    zes = new JLabel("0");
+	    this.add(zes);
+	}
+
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		int amount;
+		d = (DobbelsteenModel) e.getSource();
+		waarde = d.getWaarde();
+	
+		switch(waarde)
+		{
+			case 1: 
+				amount = Integer.parseInt(een.getText());
+				amount++;
+				een.setText(Integer.toString(amount));
+			break;
+			case 2: 
+				amount = Integer.parseInt(twee.getText());
+				amount++;
+				twee.setText(Integer.toString(amount));
+			break;
+			case 3: 
+				amount = Integer.parseInt(drie.getText());
+				amount++;
+				drie.setText(Integer.toString(amount));
+			break;
+			case 4:  
+				amount = Integer.parseInt(vier.getText());
+				amount++;
+				vier.setText(Integer.toString(amount));
+			break;
+			case 5: 
+				amount = Integer.parseInt(vijf.getText());
+				amount++;
+				vijf.setText(Integer.toString(amount));
+			break;
+			case 6:  
+				amount = Integer.parseInt(zes.getText());
+				amount++;
+				zes.setText(Integer.toString(amount));
+			break;
+		}		
+	}
+	
+	public Dimension getPreferredSize()
+	{
+	    return new Dimension(150,100);
+	} 
+}
